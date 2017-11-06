abfd9273e97659759a1de6b495bdc7bde91c50e9
Garett Roberts <mygarett@gmail.com>
2017-11-05 17:13:07 -0800

    Added scraper file

---
+++ scraper.py
@@ -0,0 +1,20 @@
+import json,time,os.path,math
+from urllib.request import urlopen
+from datetime       import date
+from rsitem         import RSItem
+from os             import remove
+
+    
+def Scraper:
+        
+    def __init__(self):
+        self.categoryurl  ="http://services.runescape.com/m=itemdb_oldschool" + \
+            "/api/catalogue/category.json?category=" 
+        self.itemgraburl = "http://services.runescape.com/m=itemdb_oldschool" + \
+            "/api/catalogue/items.json?category="
+        self.itemdetail  = "http://services.runescape.com/m=itemdb_oldschool" + \
+            "/api/catalogue/detail.json?item="
+        self.quantityurl = "http://api.rsbuddy.com/grandExchange?a=guidePric" + \
+            "e&i="
+ 
+ 
