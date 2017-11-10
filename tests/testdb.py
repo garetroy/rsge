abe0ac585084090848fcfc7111065addb49b4e06
Garett Roberts <mygarett@gmail.com>
2017-11-10 13:42:34 -0800

    Reverted back before previous commit

--- tests/testdb.py
+++ tests/testdb.py
@@ -36,23 +36,20 @@ assert (db.getCatalogueColSize() == 12), "Catalogue size is incorrect"
 
 assert (db.clearItems() == True),"Collection didn't exist!"
 
-item  = {"id":1,"name":"dog","members":True,"today":"1","date":"00/00/0000",\
-        "catid":1}
-item2 = {"id":3,"name":"dog","members":True,"today":"1","date":"00/00/0020",\
-        "catid":1}
-item3 = {"id":2,"name":"dog","members":False,"today":"1","date":"00/00/0020",\
-        "catid":1}
+item  = {"id":1,"name":"dog","members":True,"today":"1","date":"00/00/0000"}
+item2 = {"id":3,"name":"dog","members":True,"today":"1","date":"00/00/0020"}
+item3 = {"id":2,"name":"dog","members":False,"today":"1","date":"00/00/0020"}
 
 assert (db.insertItems([item,item2,item3]) == 0),\
         "Couldn't insert item into items collection"
 
 assert (db.itemQuery({"members":False}) == 
     [{'_id': 2, 'name': 'dog', 'members': False, 'today': '1', 'dateacc':\
-     '00/00/0020',"catid":1}]), "Bad item query"
+     '00/00/0020'}]), "Bad item query"
 
 desiredresult = [{'_id': 3, 'name': 'dog', 'members': True, 'today': '1',\
-     'dateacc': '00/00/0020',"catid":1}, {'_id': 2, 'name': 'dog',\
-     'members': False, 'today': '1', 'dateacc': '00/00/0020',"catid":1}]
+     'dateacc': '00/00/0020'}, {'_id': 2, 'name': 'dog', 'members': False,\
+     'today': '1', 'dateacc': '00/00/0020'}]
 
 assert (db.gatherAllItemsByDate("00/00/0020")  == desiredresult), "Bad gather"
 
