f75f30c7d9bc1cd2d0e1e5ef37456f9fc348d5ab
Garett Roberts <mygarett@gmail.com>
2017-11-22 12:32:19 -0800

    Created base scraper

--- src/scraper.py
+++ src/scraper.py
@@ -1,54 +1,38 @@
+"""
+    scraper.py
+
+    This program is for scraping the osrs api.  
+"""
 import json,time,os.path,math
 from urllib.request import urlopen
 from datetime       import date
-from rsitem         import RSItem
 from os             import remove
-
     
-def Scraper:
+class Scraper:
+    """
+        A scraper class for the program
+    """
         
     def __init__(self):
-        self.categoryurl = "http://services.runescape.com/m=itemdb_oldschool"+\
-            "/api/catalogue/category.json?category=" 
-        self.itemgraburl = "http://services.runescape.com/m=itemdb_oldschool"+\
-            "/api/catalogue/items.json?category="
+        """
+            Creates the links we need for scraping
+        """
+        #For grabbing an item from an item id
         self.itemdetail  = "http://services.runescape.com/m=itemdb_rs/api/"+\
             "catalogue/detail.json?item="
  
-    def getCategory(self,catID):
-        if type(catID) != int:
-            print("catID must be of type int in getCategory")
-            return {}
-
-        try:
-            url  = self.categoryurl + str(catID)
-            resp = urlopen(url) #add to parallelizer
-            cont = json.loads(resp.read())
-            return cont
-        except:
-            print("Couldn't find category {} in getCategory".format(catID))
-            return {} #Couldn't find page
-        
-    def getAllCategories(self):
-        #needs error checking
-        categories = []
-        
-        #For threading here... Might be a good idea to 
-        #push each url request to the parallelizer so it can
-        #handle wait times
-
-        for catID in range(1,38):
-            time.sleep(5) #Temporary, will be done by parallelizer
-            try:
-                url  = self.categoryurl+str(catID)
-                resp = urlopen(url) #goes to parallelizer, add categories
-                cont = json.loads(resp.read())
-
-                categories.append(cont) 
-            except: #Found the end of the categories
-                return categories
-
     def getItem(self,idnum):
+        """
+            Grabs the items from the given idnum
+
+            @param:
+                idnum (int) - idnum of the item we want
+
+            @returns:
+                (dict) - A dictionary representing a json request
+                        Empty if failed
+        """
+
         if type(idnum) != int:
             print("idnum must be of type int in getItem")
             return {}
@@ -57,11 +41,7 @@ def Scraper:
             url  = self.itemdetail + str(idnum)
             resp = urlopen(url) #add to parallelizer
             cont = json.loads(resp.read())
-            return cont
+            return cont['item']
         except:
             print("Could not find id {} in getItem".format(idnum))
             return {}
-
-    def getItems(self):
-        pass
-
